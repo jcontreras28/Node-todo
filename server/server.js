73f9f98f657c5e8d030c9fbb9521c5451eaef640
jcontreras28 <janellec@bendbroadband.com>
2017-12-20 16:58:48 -0800

    add post user route and token methods

--- server/server.js
+++ server/server.js
@@ -15,6 +15,8 @@ const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
+////////////////     TODOS routes /////////////////////////////////
+
 app.post('/todos', (req, res) => {
     var todo = new Todo({
         text: req.body.text
@@ -92,7 +94,29 @@ app.patch('/todos/:id', (req, res) => {
     }).catch((e) => {
         res.status(404).send();
     });
-})
+});
+
+
+////////////////     USERS routes /////////////////////////////////
+app.post('/users', (req, res) => {
+
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);        
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.get('/users', (req, res) => {
+    User.find().then((users) => {
+            res.send({users});
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
 
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
